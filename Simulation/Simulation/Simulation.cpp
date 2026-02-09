85c6d60b25ad89b78eec56874d9ad1fe93cf1340
goups33 <mmoreau@gaming.tech>
2026-02-09 17:01:03 +0100

    debut

--- Simulation/Simulation/Simulation.cpp
+++ Simulation/Simulation/Simulation.cpp
@@ -10,19 +10,23 @@ int main()
     sf::Sprite sprite(texture);
 
     sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Cyan);
+    shape.setFillColor(sf::Color::Red);
+    shape.setOutlineThickness(10.f);
+    shape.setOutlineColor(sf::Color::Red);
     // 'entity' can be a sf::Sprite, a sf::Text, a sf::Shape or any other transformable class
 
 // set the origin of the entity
     shape.setOrigin({ -200.f, -200.f });
 
+
     // retrieve the origin of the entity
-    sf::Vector2f origin = shape.getOrigin(); // = (10.f, 20.f)
+    sf::Vector2f origin = shape.getOrigin();// = (10.f, 20.f)
 
     // Create a graphical text to display
     const sf::Font font("arial.ttf");
-    sf::Text text(font, "Abonnez vous à Fufu le Gamer", 50);
+    sf::Text text(font, "fufu le gamer il est nul", 50);
 
+    text.setRotation(sf::degrees(35));
     // Start the game loop
     while (window.isOpen())
     {
