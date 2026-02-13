c3730c33a825cc354070d1516ee02eb0a3e5dbcd
Fufu-le-Gamer <yducrocq@gaming.tech>
2026-02-13 16:34:24 +0100

    we

--- Simulation/Simulation/Simulation.cpp
+++ Simulation/Simulation/Simulation.cpp
@@ -3,7 +3,6 @@
 #include <iostream>
 #include <vector>
 #include <thread>
-
 #include "BehaviorTree.h"
 #include "Node.h"
 #include "Ia.h"
@@ -73,15 +72,6 @@ int main()
     MyBt->BuildTree();
     MyBt->BeginExecute();
 
-    
-
-
-
-
-
-
-
-
     // Create the main window
     sf::ContextSettings settings;
     settings.antiAliasingLevel = 8;
@@ -100,10 +90,9 @@ int main()
     Roadx.setSize({ 1000.f, 50.f });
     Roadx.setOrigin({ 0.f, -275.f });
 
-    // define a rectangle
     sf::RectangleShape Roady({ 120.f, 50.f });
     Roady.setFillColor(sf::Color::Gray);
-    // change the size
+  
     Roady.setSize({ 1000.f, 50.f });
     Roady.setOrigin({ 0.f, 450.f });
     Roady.setRotation(sf::degrees(80));
@@ -117,9 +106,9 @@ int main()
     Buildings bakery1(
         font,
         "Bakery 1",
-        { 130.f, 250.f },
-        { -10.f, -20.f },
-        {0.f, 0.f},
+        { 130.f, 250.f },    //taill
+        { -10.f, -20.f },   //origin
+        {0.f, 0.f},         //position
         sf::Color::Red,
         0.f
     );
@@ -137,7 +126,7 @@ int main()
     Buildings butcher1(
         font,
         "Butcher 1",
-        { 130.f, 250.f },
+        { 130.f, 250.f },  
         { -300.f, -20.f },
         { 300.f, 0.f },
         sf::Color::Cyan,
@@ -174,8 +163,11 @@ int main()
         0.f
     );
 
-    Clients clients( 20.f, 300.f, 200.f); //taille, position, vitesse
+    Clients clients1( 20.f, 300.f, 200.f); //taille, position, vitesse
     sf::Clock clock;
+
+    Clients clients2(20.f, 400.f, 200.f);
+    clock;
 
     // Start the game loop
     while (window.isOpen())
@@ -193,14 +185,15 @@ int main()
 
             float deltaTime = clock.restart().asSeconds();
 
-            clients.update(deltaTime);
+            clients1.update(deltaTime);
 
             window.clear();
 
             window.draw(Roadx);
             window.draw(Roady);
             window.draw(Center);
-            clients.draw(window);
+            clients1.draw(window);
+            clients2.draw(window);
             bakery1.draw(window);
             bakery2.draw(window);
             bank.draw(window);
